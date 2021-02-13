36f0da83bdfeae7657d5314f54cbdb4067393e85
Laura Fernández García <laura@MacBook-Air-de-Laura.local>
2021-02-13 13:58:05 +0100

    V8

--- code/WGCNA_deps_R.R
+++ code/WGCNA_deps_R.R
@@ -1,3 +1,7 @@
+args = commandArgs(trailingOnly = TRUE)
+#wd <- args[1]
+software_deps <- args[1]
+
 # WGCNA: en windows
 # Es necesario actualizar R a la versión 4.0.3
 
@@ -6,34 +10,32 @@
 ################## Actualir R en windows ################
 # Instala / carga el paquete
 
-#if(!require(installr)) {
- # install.packages("installr")
-  #require(installr)
-  #} 
+if(!require(installr)) {
+  install.packages("installr", repos = 'http://cran.us.r-project.org',  lib = "software_deps")
+  require(installr)
+} 
 
-# setwd("D:/uni/2020-21/BiologiaSistemas/Practicas/Aurelio/trabajoFinal/Git/project_template_BS_SARS-CoV2/code")
-###
 # actualizar R
-#install.packages("installr")
+#install.packages("installr", repos = 'http://cran.us.r-project.org')
 
-#library("installr")
+library("installr", repos = 'http://cran.us.r-project.org',  lib = "software_deps")
 
 ########################################################
 
 
 ################### Rtool para BiocManager #######################
 if (!requireNamespace ("BiocManager", quietly = TRUE)) 
-  #if (!requireNamespace ("BiocManager", quietly = TRUE)) 
-#install.packages ("BiocManager")
- install.packages("BiocManager", repos = 'http://cran.us.r-project.org')
+  
+  install.packages("BiocManager", repos = 'http://cran.us.r-project.org', lib = "software_deps")
 
 BiocManager::install(c("RTCGAToolbox", "WGCNA", "DESeq2", "coexnet")) 
-
-install.packages ("dplyr", repos = 'http://cran.us.r-project.org')
-install.packages("factoextra", repos = 'http://cran.us.r-project.org')
-install.packages("NbClust", repos = 'http://cran.us.r-project.org')
-install.packages("DCGL", repos = 'http://cran.us.r-project.org')
-install.packages("ggplot2", repos = 'http://cran.us.r-project.org')
+# "D:/uni/2020-21/BiologiaSistemas/Practicas/Aurelio/trabajoFinal/Git/project_template_BS_SARS-CoV2/code/software/deps"
+install.packages ("dplyr", repos = 'http://cran.us.r-project.org',  lib = "software_deps")
+install.packages("factoextra", repos = 'http://cran.us.r-project.org',  lib = "software_deps")
+install.packages("NbClust", repos = 'http://cran.us.r-project.org',  lib = "software_deps")
+install.packages("DCGL", repos = 'http://cran.us.r-project.org',  lib = "software_deps")
+install.packages("ggplot2", repos = 'http://cran.us.r-project.org',  lib = "software_deps")
+install.packages("tidyverse ", repos = 'http://cran.us.r-project.org',  lib = "software_deps")
 ###########################################################
 
 ### cargamos las librerias:
@@ -46,8 +48,7 @@ library("NbClust") # Determinar el mejor número de clústeres en un conjunto de
 library("cluster") # Encontrar grupos en los datos
 library("DESeq2") # Análisis de datos de RNA-seq
 library("DCGL") # Análisis de coexpresión diferencial y análisis de regulación diferencial de 
-                # datos de microarrays de expresión génica
+# datos de microarrays de expresión génica
 
 library("coexnet") # Graficas de coexpresion de genes
-
-
+library("tidyverse")

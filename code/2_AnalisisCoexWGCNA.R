c0c52ca11268866db96504f6b19f51b5a872cd34
Mariana Gonzalez Jimenez <64373905+marianagon17@users.noreply.github.com>
2021-02-14 17:49:56 +0100

    Update 2_AnalisisCoexWGCNA.R

--- code/2_AnalisisCoexWGCNA.R
+++ code/2_AnalisisCoexWGCNA.R
@@ -7,7 +7,7 @@ WGCNA.res2[1:24]
 Links2 <- qLinkfilter(expresion3, expresion4, 0.25)
 names(Links2)
 umbral_ex3<-Links2$rth.1
-#vemos que tiene una correlacion de 0.9946
+#vemos que tiene una correlacion de 0.994
 umbral_ex4<-Links2$rth.2
 #vemos que tiene una correlacion de 0.997
 
@@ -17,18 +17,18 @@ WGCNA.res3[1:24]
 Links3 <- qLinkfilter(expresion3, expresion5, 0.25)
 names(Links3)
 umbral_ex3<-Links3$rth.1
-#vemos que tiene una correlacion de 0.9946
+#vemos que tiene una correlacion de 0.99441
 umbral_ex5<-Links3$rth.2
-#vemos que tiene una correlacion de 0.9828
+#vemos que tiene una correlacion de 0.9833
 
 WGCNA.res4 <- WGCNA(exprs.1=expresion3, exprs.2=expresion6, power = 12, variant = "WGCNA")
 WGCNA.res4[1:24]
 Links4 <- qLinkfilter(expresion3, expresion6, 0.25)
 names(Links4)
 umbral_ex3<-Links4$rth.1
-#vemos que tiene una correlacion de 0.9946
+#vemos que tiene una correlacion de 0.99441
 umbral_ex6<-Links4$rth.2
-#Vamos que tiene una correlacion de 0.9303
+#Vamos que tiene una correlacion de 0.9291
 
 
 WGCNA.res5 <- WGCNA(exprs.1=expresion3, exprs.2=expresion7, power = 12, variant = "WGCNA")
@@ -36,9 +36,9 @@ WGCNA.res5[1:24]
 Links5 <- qLinkfilter(expresion3, expresion7, 0.25)
 names(Links5)
 umbral_ex3<-Links5$rth.1
-#vemos que tiene una correlacion de 0.9946
+#vemos que tiene una correlacion de 0.99441
 umbral_ex7<-Links5$rth.2
-#Vamos que tiene una correlacion de 0.990209
+#Vamos que tiene una correlacion de 0.990
 
 
 WGCNA.res6 <- WGCNA(exprs.1=expresion3, exprs.2=expresion8, power = 12, variant = "WGCNA")
@@ -46,17 +46,20 @@ WGCNA.res6[1:24]
 Links6 <- qLinkfilter(expresion3, expresion8, 0.25)
 names(Links6)
 umbral_ex3<-Links6$rth.1
-#vemos que tiene una correlacion de 0.9946
+#vemos que tiene una correlacion de 0.99441
 umbral_ex8<-Links6$rth.2
-#Vamos que tiene una correlacion de 0.9964594
+#Vamos que tiene una correlacion de 0.996
 
 WGCNA.res7 <- WGCNA(exprs.1=expresion3, exprs.2=expresion9, power = 12, variant = "WGCNA")
 WGCNA.res7[1:24]
 Links7 <- qLinkfilter(expresion3, expresion9, 0.25)
 names(Links7)
 umbral_ex3<-Links7$rth.1
-#vemos que tiene una correlacion de 0.9946
+#vemos que tiene una correlacion de 0.99441
 umbral_ex9<-Links7$rth.2
-#Vamos que tiene una correlacion de 0.391231
+#Vamos que tiene una correlacion de 0.0569
 
-datos_expresiones<-rbind(expresion3,expresion4)
+#no ponemos la expresion 9 porque tiene una correlacion de 0.05
+datos_expresiones<-rbind(expresion3,expresion4,expresion7,expresion8)
+
+

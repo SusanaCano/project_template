3d435663fe62f7e7582b7eb955b6e420a2fba9d6
SusanaCano <75071526+SusanaCano@users.noreply.github.com>
2021-02-14 22:30:44 +0100

    5_Cytoscape.R v1_sh

--- code/5_Cytoscape.R
+++ code/5_Cytoscape.R
@@ -1,13 +1,22 @@
+args = commandArgs(trailingOnly = TRUE)
+wd <- args[2]
+software_deps <- args[3]
+results <- args[4]
+
+
+setwd(wd)
+
+source(paste(wd, "4_RGheatmap.R", sep = "/"))
+
 #######################CYTOSCAPE##########################
+
 # Hacemos una funcion que seleccione cada uno de los modulos de color
-setwd("C:/Users/Mariana/Documents/GitHub/project_template_BS_SARS-CoV2/results")
-getwd()
-for (mod in 1:nrow(table(moduleColors)))
+for (mod in 1:nrow(table (moduleColors)))
 {
   
-  modules <- names(table(moduleColors))[mod]
+  modules <- names(table (moduleColors))[mod]
   #Seleccionamos los modulos de las sondas(probes)
-  probes <- names(datos_expresiones)
+  probes <- names (datos_expresiones)
   inModule <- (moduleColors == modules)
   modProbes <- probes[inModule]
   modGenes <- modProbes
@@ -17,23 +26,25 @@ for (mod in 1:nrow(table(moduleColors)))
   dimnames(modTOM) <- list(modProbes, modProbes)
   #Exportamos la red a archivos de lista de nodos y de borde que Cytoscape puede leer 
   cyt <- exportNetworkToCytoscape(modTOM,
-                                 edgeFile = paste("CytoscapeInput-edges-", modules , ".txt", sep=""),
-                                 nodeFile = paste("CytoscapeInput-nodes-", modules, ".txt", sep=""),
-                                 weighted = TRUE,
-                                 threshold = 0.02,
-                                 nodeNames = modProbes,
-                                 altNodeNames = modGenes,
-                                 nodeAttr = moduleColors[inModule])
+                                  edgeFile = paste("CytoscapeInput-edges-", modules , ".txt", sep = ""),
+                                  nodeFile = paste("CytoscapeInput-nodes-", modules, ".txt", sep = ""),
+                                  weighted = TRUE,
+                                  threshold = 0.02,
+                                  nodeNames = modProbes,
+                                  altNodeNames = modGenes,
+                                  nodeAttr = moduleColors[inModule])
 }
 
 
 
 # Constuimos la red
+setwd(results)
 
-cor_pearson1 <- createNet(expData = datos_expresiones,threshold = 1,method = "correlation")
-plot(cor_pearson1)
+memory.limit(size = 120000)
+
+cor_pearson <- createNet(expData = datos_expresiones, threshold = 1, method = "correlation")
+plot(cor_pearson)
 jpeg("cor_pearson.jpeg")
-plot(cor_pearson1)
+plot(cor_pearson)
 dev.off()
-setwd(workingDir)
-getwd()
+setwd(wd)
